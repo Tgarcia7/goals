ffc299ed3ff999f98bdcbc23829087f77562642a
Tey García <tgarciamiranda@gmail.com>
2020-04-02 18:19:25 -0600

    testing scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    test: /\.scss$/,
-    use: [
-      'vue-style-loader',
-      'css-loader',
-      {
-        loader: 'sass-loader',
-        options: {
-          data: `
-            @import "@sweetalert2/theme-dark/dark.scss";
-          `
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
         }
-      }
-    ]
+      ]
+    }
   }
