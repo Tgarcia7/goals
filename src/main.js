d35aea40b41df94fa34b7933cbc7c7bc1c734cd7
Tey García <tgarciamiranda@gmail.com>
2020-04-05 16:34:26 -0600

    pwa support

--- src/main.js
+++ src/main.js
@@ -18,6 +18,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import './registerServiceWorker'
 
 library.add(fas, fab)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/goals/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/goals/' : '/',
+  pwa: {
+    name: 'Goals',
+    themeColor: '#343a40',
+    msTileColor: '#343a40',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    assetsVersion: '0.0.1'
+  }
 }
