03a991df82999deaba93678f9a508a2047fb5a01
Tgarcia7 <tgarciamiranda@gmail.com>
2020-04-05 22:35:59 +0000

    Deploying to gh-pages from  @ d35aea40b41df94fa34b7933cbc7c7bc1c734cd7 🚀

---
+++ precache-manifest.d844aba968e17218943c44b79c420d48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924f593288246a5ff036",
+    "url": "/goals/css/app.300ec5ba.css"
+  },
+  {
+    "revision": "2c44979aceceec528e34",
+    "url": "/goals/css/chunk-vendors.6af9d10e.css"
+  },
+  {
+    "revision": "47aa2b99fc51c03ba7a79cf090d02321",
+    "url": "/goals/index.html"
+  },
+  {
+    "revision": "924f593288246a5ff036",
+    "url": "/goals/js/app.068333e7.js"
+  },
+  {
+    "revision": "2c44979aceceec528e34",
+    "url": "/goals/js/chunk-vendors.1cf26959.js"
+  },
+  {
+    "revision": "3708bc77d6d8e6d952ca4d3216e7ab9b",
+    "url": "/goals/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/goals/robots.txt"
+  }
+]);
