717e819125b2a76ffa5ecf4728aa067d96f8fd26
Tgarcia7 <tgarciamiranda@gmail.com>
2020-04-06 07:19:17 +0000

    Deploying to gh-pages from  @ 321d583d7697ff0d447eefce7753aceb0842baa5 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,27 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts("/goals/precache-manifest.36dfe6cdd1b75119a257c90eddb8bf8c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts(
-  "/goals/precache-manifest.bc49db845f327e38e39304314ffd08d8.js"
-);
-
-workbox.core.setCacheNameDetails({prefix: "goals"});
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+import { workbox } from 'register-service-worker'
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+self.addEventListener('message', (e) => {
+    if (!e.data) {
+      return;
+    }
+  
+    switch (e.data) {
+      case 'skipWaiting':
+        self.skipWaiting();
+        break;
+      default:
+        // NOOP
+        break;
+    }
+  });
+  
+  workbox.core.clientsClaim(); // Vue CLI 4 and Workbox v4, else
+  // workbox.clientsClaim(); // Vue CLI 3 and Workbox v3.
+  
+  // The precaching code provided by Workbox.
+  self.__precacheManifest = [].concat(self.__precacheManifest || []);
+  // workbox.precaching.suppressWarnings(); // Only used with Vue CLI 3 and Workbox v3.
+  workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
