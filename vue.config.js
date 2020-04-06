449a9c4061ac917339ce19a83fde92f4601bd022
Tey García <tgarciamiranda@gmail.com>
2020-04-06 01:23:28 -0600

    pwa force update

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,9 @@ module.exports = {
     msTileColor: '#343a40',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-    assetsVersion: '0.0.3'
+    assetsVersion: '0.0.3',
+    workboxOptions: {
+      skipWaiting: true
+    }
   }
 }
