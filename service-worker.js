e3fc504afa40e719045a7b762391ef597c5659a9
Tgarcia7 <tgarciamiranda@gmail.com>
2020-04-07 06:38:23 +0000

    Deploying to gh-pages from  @ 1ba66925ae110a87ddc0dca75a7c6aa4cd4dec91 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,26 @@
-importScripts("/goals/precache-manifest.b24d02d0438023e60493ad24fa7f4e46.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/goals/precache-manifest.9620dbf7b01287a6e23dd886a56098a0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef, no-underscore-dangle, no-restricted-globals */
 
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
 self.addEventListener('message', (e) => {
-    if (!e.data) {
-      return;
-    }
+    if (!e.data) return
   
     switch (e.data) {
       case 'skipWaiting':
-        self.skipWaiting();
-        break;
+        self.skipWaiting()
+        break
       default:
         // NOOP
-        break;
+        break
     }
-  });
+  })
   
-  workbox.core.clientsClaim(); // Vue CLI 4 and Workbox v4, else
-  // workbox.clientsClaim(); // Vue CLI 3 and Workbox v3.
+  workbox.core.clientsClaim() // Vue CLI 4 and Workbox v4, else
+  // workbox.clientsClaim() // Vue CLI 3 and Workbox v3.
   
   // The precaching code provided by Workbox.
-  self.__precacheManifest = [].concat(self.__precacheManifest || []);
-  // workbox.precaching.suppressWarnings(); // Only used with Vue CLI 3 and Workbox v3.
-  workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+  self.__precacheManifest = [].concat(self.__precacheManifest || [])
+  // workbox.precaching.suppressWarnings() // Only used with Vue CLI 3 and Workbox v3.
+  workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
