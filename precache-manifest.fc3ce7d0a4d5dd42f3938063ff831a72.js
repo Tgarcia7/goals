016a9cb554f466a8296057d4b26404e01607159e
Tgarcia7 <tgarciamiranda@gmail.com>
2020-04-07 20:21:03 +0000

    Deploying to gh-pages from  @ ef45add89a0de07c79f59902233e10f9b3cefb4f 🚀

---
+++ precache-manifest.fc3ce7d0a4d5dd42f3938063ff831a72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6425baf15a27666a45",
+    "url": "/goals/css/app.99356d65.css"
+  },
+  {
+    "revision": "2c44979aceceec528e34",
+    "url": "/goals/css/chunk-vendors.6af9d10e.css"
+  },
+  {
+    "revision": "4a048ea1ea04a76851b3a1c0e373f1cd",
+    "url": "/goals/index.html"
+  },
+  {
+    "revision": "0c6425baf15a27666a45",
+    "url": "/goals/js/app.7e273514.js"
+  },
+  {
+    "revision": "2c44979aceceec528e34",
+    "url": "/goals/js/chunk-vendors.1cf26959.js"
+  },
+  {
+    "revision": "3708bc77d6d8e6d952ca4d3216e7ab9b",
+    "url": "/goals/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/goals/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/goals/precache-manifest.4e5c97f12029fc9350d1874a231d690d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/goals/precache-manifest.fc3ce7d0a4d5dd42f3938063ff831a72.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef, no-underscore-dangle, no-restricted-globals */
 
