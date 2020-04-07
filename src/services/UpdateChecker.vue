24fd711b67e31a34cb5e893c77b83759e1463ce3
Tey García <tgarciamiranda@gmail.com>
2020-04-07 13:04:40 -0600

    toast after update

--- src/services/UpdateChecker.vue
+++ src/services/UpdateChecker.vue
@@ -1,50 +1,64 @@
 <template>
-    <b-alert v-model="updateExists" dismissible
-        class="mb-2 rounded-0" style="z-index: 2000;"
-        variant="success" @click="refreshApp">
-        
-        New version available! 
-        <b-button variant="success" pill size="sm" 
-            v-if="updateExists" @click="refreshApp">Click</b-button> 
-        to update
+  <div>
+    <b-alert
+      v-model="updateExists"
+      dismissible
+      class="mb-2 rounded-0"
+      style="z-index: 2000;"
+      variant="success"
+      @click="refreshApp"
+    >
+      New version available!
+      <b-button variant="success" pill size="sm" v-if="updateExists" @click="refreshApp">Click</b-button> 
+      to update
     </b-alert>
+
+    <Swal ref="swal"/>
+  </div>
 </template>
 
 <script>
+  import Swal from "../services/Swal";
+
   export default {
-    name: 'UpdateChecker',
-    data: function () {
+    name: "UpdateChecker",
+    data: function() {
       return {
         refreshing: false,
         registration: null,
         updateExists: false
-      }
+      };
+    }, 
+    components: {
+      Swal
     },
-    created: function () {
+    created: function() {
       // Listen for swUpdated event and display refresh snackbar as required.
-      document.addEventListener('swUpdated', this.showRefreshUI, { once: true })
+      document.addEventListener("swUpdated", this.showRefreshUI, { once: true })
+      
       // Refresh all open app tabs when a new service worker is installed.
-      navigator.serviceWorker.addEventListener('controllerchange', () => {
+      navigator.serviceWorker.addEventListener("controllerchange", () => {
         if (this.refreshing) return
         this.refreshing = true
         window.location.reload()
       })
     },
     methods: {
-      showRefreshUI: function (e) {
+      showRefreshUI: function(e) {
         // Display a button inviting the user to refresh/reload the app due
         // to an app update being available.
         // The new service worker is installed, but not yet active.
         // Store the ServiceWorkerRegistration instance for later use.
         this.registration = e.detail
         this.updateExists = true
+        this.$refs.swal.toast('success', 'App updated succesfully')
       },
-      refreshApp: function () {
+      refreshApp: function() {
         // Handle a user tap on the update app button.
-        this.updateExists = false
+        this.updateExists = false;
         // Protect against missing registration.waiting.
         if (!this.registration || !this.registration.waiting) return
-        this.registration.waiting.postMessage('skipWaiting')
+        this.registration.waiting.postMessage("skipWaiting")
       }
     }
   }
