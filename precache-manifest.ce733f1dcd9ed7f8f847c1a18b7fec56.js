180ad5a5dc174cfc461e3f19810232e9c6beaef7
Tgarcia7 <tgarciamiranda@gmail.com>
2020-05-20 17:08:03 +0000

    Deploying to gh-pages from  @ dda1a57f34dc1ecfce9c6c95c7f66d973dd109e1 🚀

---
+++ precache-manifest.ce733f1dcd9ed7f8f847c1a18b7fec56.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd5dc49ebff0ec81d01",
+    "url": "/goals/css/app.c56f4128.css"
+  },
+  {
+    "revision": "bd60949345f2c912ab3b",
+    "url": "/goals/css/chunk-vendors.6af9d10e.css"
+  },
+  {
+    "revision": "4e0fd9d8283ad9faf57d101837416ebb",
+    "url": "/goals/img/logo.4e0fd9d8.png"
+  },
+  {
+    "revision": "3247dacc1107791038f4a6e45ea50db3",
+    "url": "/goals/index.html"
+  },
+  {
+    "revision": "1bd5dc49ebff0ec81d01",
+    "url": "/goals/js/app.763d47ae.js"
+  },
+  {
+    "revision": "bd60949345f2c912ab3b",
+    "url": "/goals/js/chunk-vendors.e28d5484.js"
+  },
+  {
+    "revision": "b93cb4716d6637004d395e45d15e2e98",
+    "url": "/goals/logo.png"
+  },
+  {
+    "revision": "3708bc77d6d8e6d952ca4d3216e7ab9b",
+    "url": "/goals/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/goals/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,26 @@
-importScripts("/goals/precache-manifest.3abcdb27aabb77575309e76ce11ffec2.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/goals/precache-manifest.ce733f1dcd9ed7f8f847c1a18b7fec56.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef, no-underscore-dangle, no-restricted-globals */
 
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
 self.addEventListener('message', (e) => {
-    if (!e.data) return
+  if (!e.data) return
   
-    switch (e.data) {
-      case 'skipWaiting':
-        self.skipWaiting()
-        break
-      default:
-        // NOOP
-        break
-    }
-  })
+  switch (e.data) {
+    case 'skipWaiting':
+      self.skipWaiting()
+      break
+    default:
+      // NOOP
+      break
+  }
+})
   
-  workbox.core.clientsClaim() // Vue CLI 4 and Workbox v4, else
-  // workbox.clientsClaim() // Vue CLI 3 and Workbox v3.
+workbox.core.clientsClaim() // Vue CLI 4 and Workbox v4, else
+// workbox.clientsClaim() // Vue CLI 3 and Workbox v3.
   
-  // The precaching code provided by Workbox.
-  self.__precacheManifest = [].concat(self.__precacheManifest || [])
-  // workbox.precaching.suppressWarnings() // Only used with Vue CLI 3 and Workbox v3.
-  workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+// The precaching code provided by Workbox.
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+// workbox.precaching.suppressWarnings() // Only used with Vue CLI 3 and Workbox v3.
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
