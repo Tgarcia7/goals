e902854f66476428c7ca224117df2c54fd29dc1d
Tey García <tgarciamiranda@gmail.com>
2020-05-20 00:17:40 -0600

    tile and theme color

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,11 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/goals/' : '/',
   pwa: {
     name: 'Goals',
-    themeColor: '#ffffff',
+    themeColor: '#343a40',
     msTileColor: '#ffffff',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-    assetsVersion: '0.0.7',
+    assetsVersion: '0.0.8',
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       swSrc: './src/sw.js',
