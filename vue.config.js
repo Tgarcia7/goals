8afb3ee617a0038b995dbfc2ff17f66c4be5617e
Tey García <tgarciamiranda@gmail.com>
2020-05-20 00:02:46 -0600

    logos updated

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
     msTileColor: '#343a40',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-    assetsVersion: '0.0.5',
+    assetsVersion: '0.0.6',
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       swSrc: './src/sw.js',
