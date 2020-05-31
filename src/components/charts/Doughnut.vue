f18fbd332477f5dc9a94532909e7f4c6073ca348
Tey García <tgarciamiranda@gmail.com>
2020-05-31 16:53:52 -0600

    stats completed

--- src/components/charts/Doughnut.vue
+++ src/components/charts/Doughnut.vue
@@ -6,11 +6,28 @@
     props: {
       height: { type: String, default: '250px', required: false },
       chartdata: { type: Object, required: true },
-      options: { type: Object, required: true }
+      options: { type: Object, required: true },
+      year: {type: Number, required: false, default: null}
     },
     mounted () {
+      this.calculateYear()
+      
       this.renderChart(this.chartdata, this.options)
       Chart.defaults.global.defaultFontColor = '#ffffff'
+    },
+    watch: {
+      year: function () {
+        this.calculateYear()
+        
+        this.$data._chart.update()
+      }
+    },
+    methods: {
+      calculateYear: function () {
+        if ( this.year ) {
+          this.chartdata.datasets[0].data = this.chartdata.datasets[0].dataYears[`${this.year}`]
+        }
+      }
     }
   }
 </script>
