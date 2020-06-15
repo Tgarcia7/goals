4b78ee573b30b58e67c84ba1c7cd7fc05a114cd6
Tgarcia7 <tgarciamiranda@gmail.com>
2020-06-15 03:22:10 +0000

    Deploying to gh-pages from  @ 462d424567b1186ffba1ab699cefaffdb6b8e0e3 🚀

---
+++ precache-manifest.ea3c7d75e84432d4c687193f442d7089.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548e17b1f65fbd02ab0f",
+    "url": "/goals/css/app.2aecc6c1.css"
+  },
+  {
+    "revision": "040025fd0d4ac26a99d3",
+    "url": "/goals/css/chunk-vendors.3cb806b4.css"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/goals/fonts/Quicksand-VariableFont_wght.f9baef8a.ttf"
+  },
+  {
+    "revision": "a6e759f8bb0e68f00d536ee7a337871a",
+    "url": "/goals/img/logo-login.a6e759f8.png"
+  },
+  {
+    "revision": "c56e299f7f1a51b67327ce90645cd085",
+    "url": "/goals/img/logo.c56e299f.png"
+  },
+  {
+    "revision": "4024d19393e8c8cef9df76029cd33809",
+    "url": "/goals/index.html"
+  },
+  {
+    "revision": "548e17b1f65fbd02ab0f",
+    "url": "/goals/js/app.bff62cfc.js"
+  },
+  {
+    "revision": "040025fd0d4ac26a99d3",
+    "url": "/goals/js/chunk-vendors.e0063696.js"
+  },
+  {
+    "revision": "3708bc77d6d8e6d952ca4d3216e7ab9b",
+    "url": "/goals/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/goals/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/goals/precache-manifest.fc2cfb027ec8d341e438872c7db470f3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/goals/precache-manifest.ea3c7d75e84432d4c687193f442d7089.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef, no-underscore-dangle, no-restricted-globals */
 
