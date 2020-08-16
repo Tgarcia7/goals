cfb8b8ab81994881c59757f3d8770a9f8872e476
Tey García <tgarciamiranda@gmail.com>
2020-08-15 20:58:34 -0600

    Dev and prod stages

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
+const stage = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
 const config = {
-  apiUrl: process.env.DB_HOST || 'https://goals-api-dev.herokuapp.com',
+  apiUrl: `https://goals-api-${stage}.herokuapp.com`,
   baseToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsidXNlcklkIjoiNWYxNDEwYzE2ZTFjOGQwNWM2YzY3NzA3IiwibmFtZSI6IlVzdWFyaW8gcHJ1ZWJhcyIsImVtYWlsIjoidGdhcmNpYW1pcmFuZGFAZ21haWwuY29tIiwiYWRtaW4iOjF9LCJpYXQiOjE1OTUxNTA1MjksImV4cCI6MjA2ODQ0OTcyOX0.YVVClqsvVCbTdtJsoZ8_eAx4Spf35XNH48xO0d_6c0I'
 }
 
