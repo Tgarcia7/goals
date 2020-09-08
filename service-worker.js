e6538385f932e1d9a8ced00708275616839dc200
Tgarcia7 <tgarciamiranda@gmail.com>
2020-09-08 16:08:39 +0000

    Deploying to gh-pages from  @ 158b6ecbf1a47ab23235f55e4b39f3ef0c1ba36e 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/goals/precache-manifest.027e305e8405929aed637dcd1c92cb55.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/goals/precache-manifest.ed8d1bf3fb2be4d517ff01d3d35fbafc.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef, no-underscore-dangle, no-restricted-globals */
 
