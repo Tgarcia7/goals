5cd96262578b774acea9ae47f1c942e49d4221de
Tey García <tgarciamiranda@gmail.com>
2020-09-07 23:40:54 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const stage = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
 const config = {
   apiUrl: `https://goals-api-${stage}.herokuapp.com`,
-  baseToken: process.env.API_TOKEN || process.env.VUE_APP_API_TOKEN
+  baseToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsidXNlcklkIjoiNWYzOTc3YjdiNWM2ZTRmZTkxZTU0NmI1IiwibmFtZSI6IlVzdWFyaW8gcHJ1ZWJhcyIsImVtYWlsIjoidGdhcmNpYW1pcmFuZGEyQGdtYWlsLmNvbSIsImFkbWluIjoxfSwiaWF0IjoxNTk3NjAxNzE5LCJleHAiOjMzMTIyODg4MTE5fQ.UeFymNsmC_rjxR_JTLIBrI57Z4AlUB0-XlDp84-XDac'
 }
-
+//process.env.API_TOKEN || process.env.VUE_APP_API_TOKEN
 export default config
