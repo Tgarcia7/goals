e0c1acaa7b45d3401ae35c4848d9d17dfbd3e9a0
Tey García <tgarciamiranda@gmail.com>
2020-09-08 09:43:16 -0600

    env vars name

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const stage = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
 const config = {
   apiUrl: `https://goals-api-${stage}.herokuapp.com`,
-  baseToken: process.env.API_TOKEN || process.env.VUE_APP_API_TOKEN
+  baseToken: process.env.VUE_APP_API_TOKEN
 }
 
 export default config
--- src/services/auth-config.js
+++ src/services/auth-config.js
@@ -1,16 +1,16 @@
 const config = {
   'auth': {
     'google': {
-      'clientId': process.env.API_GOOGLE_ID || process.env.VUE_APP_API_GOOGLE_ID,
-      'clientSecret': process.env.API_GOOGLE_SECRET || process.env.VUE_APP_API_GOOGLE_SECRET
+      'clientId': process.env.VUE_APP_API_GOOGLE_ID,
+      'clientSecret': process.env.VUE_APP_API_GOOGLE_SECRET
     },
     'facebook': {
-      'clientId': process.env.API_FACEBOOK_ID || process.env.VUE_APP_API_FACEBOOK_ID,
-      'clientSecret': process.env.API_FACEBOOK_SECRET || process.env.VUE_APP_API_FACEBOOK_SECRET
+      'clientId': process.env.VUE_APP_API_FACEBOOK_ID,
+      'clientSecret': process.env.VUE_APP_API_FACEBOOK_SECRET
     },
     'github': {
-      'clientId': process.env.API_GH_ID || process.env.VUE_APP_API_GH_ID,
-      'clientSecret': process.env.API_GH_SECRET || process.env.VUE_APP_API_GH_SECRET
+      'clientId': process.env.VUE_APP_API_GH_ID,
+      'clientSecret': process.env.VUE_APP_API_GH_SECRET
     }
   }
 }
